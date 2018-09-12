f1820f7a21e1402312f6dfa647a76115e8619649
Cloud Strife <mokryan17@gmail.com>
2018-09-12 18:39:45 -0500

    Testing heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "123456",
     database: "burger_db"

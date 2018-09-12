609e92a30f3dd209c06a01a164a39c59dfd8b091
Cloud Strife <mokryan17@gmail.com>
2018-09-12 18:25:49 -0500

    Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "123456",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "burger_db"
+  });
+};
 
 // Make connection.
 connection.connect((err) => {
